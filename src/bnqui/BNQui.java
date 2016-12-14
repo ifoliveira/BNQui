eef2c4d607c64925541bcc5653d4fbcfe9d639a0
igfernan <igfernan@LES003197.corp.capgemini.com>
2016-12-14 17:25:15 +0100

    #1: Base

---
+++ src/bnqui/BNQui.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bnqui;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author igfernan
+ */
+public class BNQui {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       JFrame inicio = new Inicio();
+       inicio.setVisible(true);
+    }
+    
+}
